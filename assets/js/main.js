9179e1f1236d51b3dddfdaeb788ee9c2a45dc9da
tonyeighteen <levuhoanganh94@gmail.com>
2023-05-04 17:48:59 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,3 +83,17 @@ $("#bootstrapForm").submit(function(event) {
 	
  
 });
+
+
+function removejscssfile(filename, filetype){
+    var targetelement=(filetype=="js")? "script" : (filetype=="css")? "link" : "none" //determine element type to create nodelist from
+    var targetattr=(filetype=="js")? "src" : (filetype=="css")? "href" : "none" //determine corresponding attribute to test for
+    var allsuspects=document.getElementsByTagName(targetelement)
+    for (var i=allsuspects.length; i>=0; i--){ //search backwards within nodelist for matching elements to remove
+    if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1)
+        allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
+    }
+}
+ 
+removejscssfile("somescript.js", "js") //remove all occurences of "somescript.js" on page
+removejscssfile("55013136-widget_css_bundle.css", "css") //remove all occurences "somestyle.css" on page
