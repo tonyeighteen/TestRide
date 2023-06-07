1fa77bfb194089a9ae8bfdf4a26764b33385ad61
tonyeighteen <levuhoanganh94@gmail.com>
2023-06-07 16:36:55 +0700

    update

--- assets/js/main-en.js
+++ assets/js/main-en.js
@@ -3,7 +3,7 @@ var dd = String(date.getDate()).padStart(2, '0');
 var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = date.getFullYear();
 
-today = mm + '/' + dd + '/' + yyyy;
+today = dd + '/' + mm + '/' + yyyy;
 var time = date.toLocaleTimeString();
 
 var dateTime = today + " at " + time;
